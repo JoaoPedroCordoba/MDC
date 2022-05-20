69d7f0590caf850edc62f7f1a8e97286c6e1d31c
JoaoPedrocordoba <jp.8pedro86@gmail.com>
2022-05-20 20:31:19 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int dividendo,  divisor, quociente, resto;
+
+Console.Clear();
+
+Console.WriteLine("Máximo Divisor Comum (método iterativo)");
+
+Console.WriteLine();
+
+Console.WriteLine("Digite o dividendo:");
+dividendo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o divisor:");
+divisor = Convert.ToInt32(Console.ReadLine());
+
+resto = dividendo % divisor;
+
+while (resto == 0)
+quociente = dividendo / divisor;
+resto = dividendo % divisor;
+
+if (resto == 0)
+{
+    Console.WriteLine($"MDC(a, b) = {dividendo}");
+}
+else
+{
+    dividendo =  divisor;
+    divisor = resto;
+    resto = dividendo % divisor;
+    Console.WriteLine($"MDC(a, b) = {resto}");
+}
